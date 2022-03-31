d94222306b002d0990200a0c282e15d0bd5ba56d
edwin carrillo <edwincarrillo9@outlook.com>
2022-03-31 12:24:09 -0400

    session updates

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -36,5 +36,25 @@ router.post(
     })
   );
 
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
 
 module.exports = router;
