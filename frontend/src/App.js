0c4db9d12ef4e9848e317458c8e6fb4d1a6ce4b7
edwin carrillo <edwincarrillo9@outlook.com>
2022-04-03 18:18:14 -0400

    touch ups on signup modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -15,15 +14,6 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/signup">
-            <div className="display">
-            <SignupFormPage />
-            </div>
-          </Route>
-        </Switch>
-      )}
     </>
   );
 }
